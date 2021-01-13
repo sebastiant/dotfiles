21c1e102fc89aac4955a77739efba34e403f3a62
Sebastian Tunstig <sebastian.tunstig@gmail.com>
2021-01-13 21:12:11 +0100

    emacs replace helm with ivy

--- init.el
+++ init.el
@@ -17,18 +17,31 @@
 (scroll-bar-mode -1)
 (setq initial-scratch-message nil)
 
-;; Helm
-(use-package helm
-  :demand
-  :bind (("M-x" . helm-M-x)
-         ("C-x C-f" . helm-find-files)
-         ("C-x b" . helm-buffers-list)
-         ("C-x c o" . helm-occur))
-         ("M-y" . helm-show-kill-ring)
-         ("C-x r b" . helm-filterd-bookmarks)
-  :preface (require 'helm-config)
-  :config (helm-mode 1))
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
 
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+         ("C-x b" . counsel-ibuffer)
+         ("C-x C-f" . counsel-find-file)
+         ("C-M-j" . counsel-find-file)
+         :map minibuffer-local-map
+         ("C-r" . 'counsel-minibuffer-history)))
 ;; magit
 (use-package magit
   :init (global-set-key (kbd "C-x g") 'magit-status))
