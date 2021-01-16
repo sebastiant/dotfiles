46f679f34d50f4007990efacf81ebd4c7b9a16e6
Sebastian Tunstig <sebastian.tunstig@gmail.com>
2021-01-16 17:06:19 +0100

    emacs: refactor

--- init.el
+++ init.el
@@ -233,7 +233,6 @@
 (use-package dap-mode)
 
 (use-package lsp-python-ms
-  :ensure t
   :init (setq lsp-python-ms-auto-install-server t)
   :hook (python-mode . (lambda ()
                           (require 'lsp-python-ms))))
