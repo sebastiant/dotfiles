ecf3637b97210284b422f773257bab67a421018e
Sebastian Tunstig <sebastian.tunstig@gmail.com>
2021-04-26 08:42:43 +0200

    emacs: add minions and elfeed

--- init.el
+++ init.el
@@ -417,6 +417,12 @@
 
 (use-package yaml-mode)
 
+(use-package minions
+  :config (minions-mode 1))
+
+(use-package elfeed
+  :config (setq elfeed-feeds '("https://hnrss.org/best")))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -425,6 +431,6 @@
  '(custom-safe-themes
    '("aaa4c36ce00e572784d424554dcc9641c82d1155370770e231e10c649b59a074" "08a27c4cde8fcbb2869d71fdc9fa47ab7e4d31c27d40d59bf05729c4640ce834" default))
  '(package-selected-packages
-   '(yaml-mode forge lsp-haskell envrc nix-mode undo-tree evil-nerd-commenter flycheck beacon vterm general dired-single alchemist perspective elixir-mode js2-mode typescript-mode git-gutter org-mode lsp-pyright ivy-prescient web-mode company-box company no-littering dap-mode lsp-treemacs lsp-ivy lsp-ui lsp-mode pyvenv python-mode haskell-mode hindent which-key doom-themes doom-modeline all-the-icons counsel-projectile projectile evil-collection evil rainbow-delimiters magit counsel ivy use-package)))
+   '(elfeed py-isort minions yaml-mode forge lsp-haskell envrc nix-mode undo-tree evil-nerd-commenter flycheck beacon vterm general dired-single alchemist perspective elixir-mode js2-mode typescript-mode git-gutter org-mode lsp-pyright ivy-prescient web-mode company-box company no-littering dap-mode lsp-treemacs lsp-ivy lsp-ui lsp-mode pyvenv python-mode haskell-mode hindent which-key doom-themes doom-modeline all-the-icons counsel-projectile projectile evil-collection evil rainbow-delimiters magit counsel ivy use-package)))
 (provide 'init)
 ;;; init.el ends here
