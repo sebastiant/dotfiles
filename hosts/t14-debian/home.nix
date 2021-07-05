f48739e3f6ce47b7c2596302614448356e0dc432
Sebastian Tunstig <sebastian.tunstig@gmail.com>
2021-07-05 21:45:11 +0200

    Use native compiled emacs using nix-community's overlay on t14

--- hosts/t14-debian/home.nix
+++ hosts/t14-debian/home.nix
@@ -1,5 +1,15 @@
 { config, pkgs, ... }:
-{
+let
+  unstable = import (fetchTarball
+  "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz") {
+    overlays = [
+      (import (builtins.fetchTarball {
+        url = https://github.com/nix-community/emacs-overlay/archive/master.tar.gz;
+      }))
+    ];
+  };
+
+in {
   programs.home-manager.enable = true;
   imports = [
     ../common.nix
@@ -7,6 +17,8 @@
   ];
   home.username = "sebastian";
   home.homeDirectory = "/home/sebastian";
+
+  programs.emacs.package = unstable.emacsGcc;
 
   home.packages = with pkgs; [
     zlib
