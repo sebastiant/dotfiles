bfe43150aef60211f6fbc5065f627d44cdc2e350
Sebastian Tunstig <sebastian.tunstig@gmail.com>
2021-08-07 15:55:56 +0200

    Remove emacs custom-set-variables

--- programs/emacs/init.el
+++ programs/emacs/init.el
@@ -435,15 +435,5 @@
 
 (use-package terraform-mode)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("aaa4c36ce00e572784d424554dcc9641c82d1155370770e231e10c649b59a074" "08a27c4cde8fcbb2869d71fdc9fa47ab7e4d31c27d40d59bf05729c4640ce834" default))
- '(flycheck-flake8rc "setup.cfg")
- '(package-selected-packages
-   '(editorconfig python-django helpful persp-projectile elfeed py-isort minions yaml-mode forge lsp-haskell envrc nix-mode undo-tree evil-nerd-commenter flycheck beacon vterm general dired-single alchemist perspective elixir-mode js2-mode typescript-mode git-gutter org-mode lsp-pyright ivy-prescient web-mode company-box company no-littering dap-mode lsp-treemacs lsp-ivy lsp-ui lsp-mode pyvenv python-mode haskell-mode hindent which-key doom-themes doom-modeline all-the-icons counsel-projectile projectile evil-collection evil rainbow-delimiters magit counsel ivy use-package)))
 (provide 'init)
 ;;; init.el ends here
