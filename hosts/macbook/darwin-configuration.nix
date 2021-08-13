5942c698777367a904a5e52a3f0234b6fe21d9cf
Sebastian Tunstig <sebastian.tunstig@gmail.com>
2021-08-13 19:37:24 +0200

    Use local repository for nix-darwin instead of channel

--- hosts/macbook/darwin-configuration.nix
+++ hosts/macbook/darwin-configuration.nix
@@ -1,6 +1,7 @@
 { config, pkgs, ... }:
 {
   nixpkgs.config.allowUnfree = true;
+  nix.nixPath = [ "darwin=/Users/sebastian/.nix-defexpr/darwin" ];
   imports = [
     <home-manager/nix-darwin>
     ./home.nix
