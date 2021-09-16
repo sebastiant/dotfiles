592c899b045f0e0b6099f97b4f877129263cc2f4
Sebastian Tunstig <sebastian.tunstig@gmail.com>
2021-09-16 22:19:11 +0200

    Fix path to configuration.nix in flake

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     nixosConfigurations.t14 = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        ./configuration.nix
+        ./hosts/t14-nixos/configuration.nix
         home-manager.nixosModules.home-manager {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
