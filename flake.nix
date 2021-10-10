ca6f4ca994cb7bda5836f95ea8377560b5a01a79
Sebastian Tunstig <sebastian.tunstig@gmail.com>
2021-10-10 14:05:25 +0200

    Refactor flake

--- flake.nix
+++ flake.nix
@@ -15,37 +15,46 @@
   outputs = {emacs-overlay, darwin, home-manager, nixpkgs, ...}:
     let
       homeManagerConfFor = config: { ... }: {
-          nixpkgs.overlays = [ emacs-overlay.overlay ];
-          imports = [ config ];
-        };
-    in {
-    nixosConfigurations.t14 = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./hosts/t14-nixos/configuration.nix
-        home-manager.nixosModules.home-manager {
-          home-manager.useUserPackages = true;
-          home-manager.users.sebastian = homeManagerConfFor ./hosts/t14-nixos/home.nix;
-        }
-      ];
-    };
-    homeManagerConfigurations = {
-      t14-debian = home-manager.lib.homeManagerConfiguration {
-        home-manager.users.sebastian = homeManagerConfFor ./hosts/t14-nixos/home.nix;
-        system = "x86_64-linux";
-        homeDirectory = "/home/sebastian";
-        username = "sebastian";
-        stateVersion = "21.05";
+        nixpkgs.overlays = [ emacs-overlay.overlay ];
+        imports = [ config ];
       };
-      macbook = darwin.lib.darwinSystem {
-        system = "x86_64-darwin";
+    in {
+      nixosConfigurations.t14 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
         modules = [
-          ./hosts/macbook/darwin-configuration.nix
-          home-manager.darwinModules.home-manager {
-            home-manager.users.sebastian = homeManagerConfFor ./hosts/macbook/home.nix;
+          ./hosts/t14-nixos/configuration.nix
+          home-manager.nixosModules.home-manager {
+            home-manager.useUserPackages = true;
+            home-manager.users.sebastian = homeManagerConfFor ./hosts/t14-nixos/home.nix;
           }
         ];
       };
+      homeManagerConfigurations = {
+        t14-debian = home-manager.lib.homeManagerConfiguration {
+          home-manager.users.sebastian = homeManagerConfFor ./hosts/t14-nixos/home.nix;
+          system = "x86_64-linux";
+          homeDirectory = "/home/sebastian";
+          username = "sebastian";
+          stateVersion = "21.05";
+        };
+        macbook = darwin.lib.darwinSystem {
+          system = "x86_64-darwin";
+          modules = [
+            ./hosts/macbook/darwin-configuration.nix
+            home-manager.darwinModules.home-manager {
+              home-manager.users.sebastian = homeManagerConfFor ./hosts/macbook/home.nix;
+            }
+          ];
+        };
+        packages.x86_64-darwin.defaultPackage = darwin.lib.darwinSystem {
+          system = "x86_64-darwin";
+          modules = [
+            ./hosts/macbook/darwin-configuration.nix
+            home-manager.darwinModules.home-manager {
+              home-manager.users.sebastian = homeManagerConfFor ./hosts/macbook/home.nix;
+            }
+          ];
+        };
+      };
     };
-  };
 }
