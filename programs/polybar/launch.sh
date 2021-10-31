b7b6c4917a12f38f7583a44cdf7794156d8a2d85
Sebastian Tunstig <sebastian.tunstig@gmail.com>
2021-10-31 11:05:40 +0100

    Merge polybars to one

--- programs/polybar/launch.sh
+++ programs/polybar/launch.sh
@@ -5,9 +5,4 @@ killall -q polybar
 # If all your bars have ipc enabled, you can also use
 # polybar-msg cmd quit
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar right 2>&1 | tee -a /tmp/polybarRight.log & disown
-polybar left 2>&1 | tee -a /tmp/polybarLeft.log & disown
-
-echo "Bars launched..."
+polybar top 2>&1 | tee -a /tmp/polybar.log & disown
