637a0f0b38c7492ef0ea7ac9134611f02d8484e2
Sebastian Tunstig <sebastian.tunstig@gmail.com>
2022-02-01 14:19:47 +0100

    Add emacs smartparens package

--- programs/emacs/emacs.nix
+++ programs/emacs/emacs.nix
@@ -61,6 +61,7 @@
       rainbow-delimiters
       restclient
       ripgrep
+      smartparens
       terraform-mode
       typescript-mode
       undo-tree
--- programs/emacs/init.el
+++ programs/emacs/init.el
@@ -631,5 +631,15 @@
   (popper-mode +1)
   (popper-echo-mode +1))
 
+(use-package smartparens
+  :config (progn (show-smartparens-global-mode t))
+  :hook ((elm-mode . turn-on-smartparens-strict-mode)
+         (emacs-lisp-mode . turn-on-smartparens-strict-mode)
+         (haskell-mode . turn-on-smartparens-strict-mode)
+         (js2-mode . turn-on-smartparens-strict-mode)
+         (typescript-mode . turn-on-smartparens-strict-mode)
+         (nix-mode . turn-on-smartparens-strict-mode)
+         (python-mode . turn-on-smartparens-strict-mode)))
+
 (provide 'init)
 ;;; init.el ends here
