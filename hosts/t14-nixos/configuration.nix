c5d6183c90345f63413b3fcca67d9761aa717614
Sebastian Tunstig <sebastian.tunstig@gmail.com>
2022-03-14 19:52:07 +0100

    Set up dropbox in NixOS

--- hosts/t14-nixos/configuration.nix
+++ hosts/t14-nixos/configuration.nix
@@ -121,7 +121,31 @@
     virtualbox.host.enable = true;
   };
 
+  networking.firewall = {
+    allowedTCPPorts = [ 17500 ];
+    allowedUDPPorts = [ 17500 ];
+  };
+
+  systemd.user.services.dropbox = {
+    description = "Dropbox";
+    wantedBy = [ "graphical-session.target" ];
+    environment = {
+      QT_PLUGIN_PATH = "/run/current-system/sw/" + pkgs.qt5.qtbase.qtPluginPrefix;
+      QML2_IMPORT_PATH = "/run/current-system/sw/" + pkgs.qt5.qtbase.qtQmlPrefix;
+    };
+    serviceConfig = {
+      ExecStart = "${pkgs.dropbox.out}/bin/dropbox";
+      ExecReload = "${pkgs.coreutils.out}/bin/kill -HUP $MAINPID";
+      KillMode = "control-group"; # upstream recommends process
+      Restart = "on-failure";
+      PrivateTmp = true;
+      ProtectSystem = "full";
+      Nice = 10;
+    };
+  };
+
   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "dropbox"
     "skypeforlinux"
     "slack"
     "spotify"
