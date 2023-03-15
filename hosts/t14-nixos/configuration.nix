65e732131d1c45a924acd4bad6fc886ea1124251
Sebastian Tunstig <sebastian.tunstig@gmail.com>
2023-03-15 22:15:01 +0100

    Enable nix-community's binary cache

--- hosts/t14-nixos/configuration.nix
+++ hosts/t14-nixos/configuration.nix
@@ -143,6 +143,16 @@
     nixPath = [ "nixpkgs=${nixpkgs}" ];
     registry.nixpkgs.flake = nixpkgs;
     package = pkgs.nixUnstable;
+    settings = {
+      substituters = [
+        "https://nix-community.cachix.org/"
+        "https://cache.nixos.org/"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+    };
+
   };
 
   fonts.fonts = with pkgs; [ font-awesome iosevka aileron ];
