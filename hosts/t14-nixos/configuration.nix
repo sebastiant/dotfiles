f8f963c2ee911b734e6e9f524cd5a2fd9689a5e5
Sebastian Tunberg <sebastian.tunberg@gmail.com>
2024-01-04 20:49:25 +0100

    Set up iohk binary cache

--- hosts/t14-nixos/configuration.nix
+++ hosts/t14-nixos/configuration.nix
@@ -163,9 +163,11 @@
       substituters = [
         "https://nix-community.cachix.org/"
         "https://cache.nixos.org/"
+        "https://cache.iog.io"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
       ];
     };
 
