e960cbc983615c1b681a767bf96d96ff54ea39f8
Sebastian Tunberg <sebastian.tunberg@gmail.com>
2024-01-04 20:49:44 +0100

    Correct wayland env var setup

--- hosts/t14-nixos/configuration.nix
+++ hosts/t14-nixos/configuration.nix
@@ -99,8 +99,8 @@
       export QT_QPA_PLATFORM=wayland-egl
       export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
       export _JAVA_AWT_WM_NONREPARENTING=1
-      export MOZ_ENABLE_WAYLAND = "1";
-      export XDG_CURRENT_DESKTOP = "sway";
+      export MOZ_ENABLE_WAYLAND=1;
+      export XDG_CURRENT_DESKTOP=sway;
     '';
   };
   programs.waybar.enable = true;
