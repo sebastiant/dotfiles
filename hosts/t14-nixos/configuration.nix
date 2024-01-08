5488fc53ecea031cc75939d9a2a14fb4f466e9d6
Sebastian Tunberg <sebastian.tunberg@gmail.com>
2024-01-08 16:45:07 +0100

    Enable allow-import-from-derivation in NixOS

--- hosts/t14-nixos/configuration.nix
+++ hosts/t14-nixos/configuration.nix
@@ -171,6 +171,7 @@
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
       ];
+      allow-import-from-derivation = "true";
     };
 
   };
