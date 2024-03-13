b9d526f6bbaa3dd239dcad67fc6a8e9c899312e6
Sebastian Tunberg <sebastian.tunberg@gmail.com>
2024-03-13 11:28:01 +0100

    Use mickeyp's treesit-language-source-alist for now

--- programs/emacs/init.el
+++ programs/emacs/init.el
@@ -208,6 +208,22 @@
 (use-package treesit
   :custom
   (treesit-font-lock-level 4)
+  (treesit-language-source-alist
+   '((bash "https://github.com/tree-sitter/tree-sitter-bash")
+     (cmake "https://github.com/uyha/tree-sitter-cmake")
+     (css "https://github.com/tree-sitter/tree-sitter-css")
+     (elisp "https://github.com/Wilfred/tree-sitter-elisp")
+     (go "https://github.com/tree-sitter/tree-sitter-go")
+     (html "https://github.com/tree-sitter/tree-sitter-html")
+     (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src")
+     (json "https://github.com/tree-sitter/tree-sitter-json")
+     (make "https://github.com/alemuller/tree-sitter-make")
+     (markdown "https://github.com/ikatyang/tree-sitter-markdown")
+     (python "https://github.com/tree-sitter/tree-sitter-python")
+     (toml "https://github.com/tree-sitter/tree-sitter-toml")
+     (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
+     (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
+     (yaml "https://github.com/ikatyang/tree-sitter-yaml")))
   (major-mode-remap-alist
    '((bash-mode . bash-ts-mode)
      (css-mode . css-ts-mode)
